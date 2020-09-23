0a365dc354edab98bba56de7a138c7e979cfda6b
Leo40Git <7997354+Leo40Git@users.noreply.github.com>
2020-09-23 13:04:11 +0300

    NOW RESTARTING, please hold...

--- src/main/java/adudecalledleo/dontdropit/api/ContainerScreenExtensions.java
+++
@@ -1,8 +0,0 @@
-package adudecalledleo.dontdropit.api;
-
-import net.minecraft.screen.slot.Slot;
-
-public interface ContainerScreenExtensions {
-    Slot getFocusedSlot();
-    void drop(boolean entireStack);
-}
